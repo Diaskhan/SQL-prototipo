2716263a6850348e6ab31bd5a8a7fb5455493043
Dias Khan <diaskhan@yandex.kz>
2026-04-19 22:45:35 +0500

    error fix

--- SQL prototipo/MainForm.Designer.cs
+++ SQL prototipo/MainForm.Designer.cs
@@ -50,7 +50,6 @@
             treeView1.Name = "treeView1";
             treeView1.Size = new Size(324, 577);
             treeView1.TabIndex = 0;
-            treeView1.AfterSelect += treeView1_AfterSelect;
             // 
             // panel1
             // 
